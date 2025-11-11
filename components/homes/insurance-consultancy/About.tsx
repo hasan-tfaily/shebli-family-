8c3325176a40ad8e12c42a4224fcb532b9d2eac9
Nabih Attar <nabihattarr@gmail.com>
2025-11-11 10:40:17 +0200

    testimonials

--- components/homes/insurance-consultancy/About.tsx
+++ components/homes/insurance-consultancy/About.tsx
@@ -19,13 +19,13 @@ export default function About() {
         </div>
         <div className="section-about-content">
           <div className="heading-section style-color-white mb-60">
-            <div className="wow fadeInUp">
+            {/* <div className="wow fadeInUp">
               <a href="#" className="tag label text-btn-uppercase bg-white">
                 WE ARE FinWice
               </a>
-            </div>
+            </div> */}
             <h2 className="mb-28 title-section wow fadeInUp">
-              Empowering Your Business Success
+              Operating Advantages:
             </h2>
             <div className="sub-title mb-32 wow fadeInUp">
               At FinWice, we empower businesses with tailored strategies,
@@ -33,14 +33,49 @@ export default function About() {
               professionals drive growth and guide you through challenges to
               achieve success.
             </div>
-            <div className="wow fadeInUp">
+
+             <div className="benefit-lists mb-32">
+                <div className="benefit-items style-small mb-16">
+                  <div className="icon wow fadeInUp">
+                    <i className="icon-checkbox" />
+                  </div>
+                  <div className="title wow fadeInUp" data-wow-delay=".1s" style={{color: "white"}}>
+                    Operational excellence and quality assurance
+                  </div>
+                </div>
+                <div className="benefit-items style-small mb-16">
+                  <div className="icon wow fadeInUp">
+                    <i className="icon-checkbox" />
+                  </div>
+                  <div className="title wow fadeInUp" data-wow-delay=".1s" style={{color: "white"}}>
+                    Safety and compliance leadership
+                  </div>
+                </div>
+                <div className="benefit-items style-small mb-16">
+                  <div className="icon wow fadeInUp">
+                    <i className="icon-checkbox" />
+                  </div>
+                  <div className="title wow fadeInUp" data-wow-delay=".1s" style={{color: "white"}}>
+                    Certified educational curriculum
+                  </div>
+                </div>
+                <div className="benefit-items style-small mb-16">
+                  <div className="icon wow fadeInUp">
+                    <i className="icon-checkbox" />
+                  </div>
+                  <div className="title wow fadeInUp" data-wow-delay=".1s" style={{color: "white"}}>
+                    Strategic partnerships and sponsorship opportunities
+                  </div>
+                </div>
+              </div>
+            {/* <div className="wow fadeInUp">
               <Link
                 href={`/about-us`}
                 className="tf-btn style-1 bg-white bg-white-style-2"
               >
                 <span> About Us </span>
               </Link>
-            </div>
+            </div> */}
           </div>
           <div className="wg-counter justify-content-between">
             {counters2.map((counter, index) => (
